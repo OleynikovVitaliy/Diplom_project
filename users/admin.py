ace8b376c11c74f68f35d9a45264f5a67544d7b5
OleynikovVitaliy <Oleinikov.Vitalik84@gmail.com>
2024-06-26 22:16:47 +0500

    Написан код проекта, сделаны миграции.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'phone', 'email', 'city', 'is_active')
+    list_filter = ('city', 'is_active')
